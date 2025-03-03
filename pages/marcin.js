ecbd7333a8a889f9086abe987c4e531f27e60ef1
kopokczo <141357204+kopokczo@users.noreply.github.com>
2025-03-03 13:21:05 +0100

    Add files via upload

---
+++ pages/marcin.js
@@ -0,0 +1,11 @@
+export default function Marcin() {
+    return (
+      <div>
+        <h1>Stronka he</h1>
+        <a href='https://google.com'>
+            Google
+        </a>
+      </div>
+    );
+  }
+  
