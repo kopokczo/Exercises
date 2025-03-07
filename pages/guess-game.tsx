30b7c5328550245ed56f0b141f8204d39d493ce8
kopokczo <141357204+kopokczo@users.noreply.github.com>
2025-03-07 13:43:52 +0100

    added reset button (#7)
    * added reset button
    
    * program refactored, everything inside of the component
    
    * resetting works
    
    * everything works
    
    * refactor
    
    * Added eslint via command
    
    * dupa
    
    * rurapcv
    
    * rower
    
    ---------
    
    Co-authored-by: bartekguz <bartoszguzik0@gmail.com>

--- pages/guess-game.tsx
+++ pages/guess-game.tsx
@@ -1,33 +1,41 @@
-import React, { useRef, useState } from "react"
+import React, { useRef, useState } from "react";
 
-const randomNumber = Math.floor(Math.random() * (20 - 1) + 1);
 const GuessGame = () => {
-    const [outputText, setOutputText] = useState<null|string>(null);
-    const [inputValue, setInputValue] = useState<null|string>(null);
-    const button = useRef(null);
-    
-    console.log(inputValue);
-    console.log(randomNumber);
-const checkNumber = () => {
+  const [outputText, setOutputText] = useState<null | string>(null);
+  const [inputValue, setInputValue] = useState<null | string>(null);
 
-        if(randomNumber > Number(inputValue)){
-            setOutputText("podana liczba jest za mala");
-        }else if(randomNumber < Number(inputValue)){
-            setOutputText("podana liczba jest za duza");
-        }else if(randomNumber == Number(inputValue)){
-            setOutputText("zgadles");
-        }
+  const randomNumber = useRef(Math.floor(Math.random() * 20) + 1);
+
+  const checkNumber = () => {
+    if (randomNumber.current > Number(inputValue)) {
+      setOutputText("podana liczba jest za mala");
+    } else if (randomNumber.current < Number(inputValue)) {
+      setOutputText("podana liczba jest za duza");
+    } else if (randomNumber.current == Number(inputValue)) {
+      setOutputText("zgadles");
     }
-    return(
+  };
+  return (
     <div>
-    <h1>zgadnij liczbe (1-20)</h1>
-    <input type="number" onChange={(event)=>setInputValue(event.target.value)}></input>
-    <button type="button" onClick={checkNumber}>submit</button>
-    <p>{outputText}</p>
+      <h1>zgadnij liczbe (1-20)</h1>
+      <input
+        type="number"
+        onChange={(event) => setInputValue(event.target.value)}
+      ></input>
+      <button type="button" onClick={checkNumber}>
+        submit
+      </button>
+      <button
+        type="button"
+        onClick={() => {
+          randomNumber.current = Math.floor(Math.random() * (20 - 1) + 1);
+        }}
+      >
+        reset
+      </button>
+      <p>{outputText}</p>
     </div>
-        )
-}
+  );
+};
+
 export default GuessGame;
-/*
-DLACZEGO JAK DASZ SUBMIT TO LOSUJE NOWA LICZBE //// losowanie liczb powinno sie odpywac poza renderowaną przez useState funkcją
-*/ 
