81c0a6be2b5236f64f80c1fd434b2b1e82e813f7
kopokczo <141357204+kopokczo@users.noreply.github.com>
2025-03-08 14:36:13 +0100

    Ex1 (#8)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,5 @@
-module.exports = {
-  extends: ["next/core-web-vitals", "prettier"],
-  plugins: ["prettier"],
-  rules: {
-    "prettier/prettier": "error", // Makes Prettier errors visible in ESLint
-    "no-console": "warn",
-    "react-hooks/exhaustive-deps": "warn",
-    "react/no-unstable-nested-components": "warn",
-    "consistent-return": "error",
-    "no-unused-vars": "error",
-  },
-};
+{
+  "extends": ["next", "next/core-web-vitals", "prettier"],
+  "plugins": [],
+  "rules": {}
+}
