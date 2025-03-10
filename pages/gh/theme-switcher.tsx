32cd115134d66be7cdeaebfcfcd52daadba5f35c
kopokczo <kodakwow@gmail.com>
2025-03-10 11:20:16 +0100

    Language changing added

--- pages/gh/theme-switcher.tsx
+++ pages/gh/theme-switcher.tsx
@@ -4,14 +4,22 @@ const ThemeContext = createContext({
   theme: "light",
   setTheme: (theme: string) => {},
 });
+const LangContext = createContext({
+  theme: "pl",
+  setTheme: (lang: string) => {},
+});
 
 const ThemeProvider = ({ children }) => {
   const [theme, setTheme] = useState("light");
+  const [lang, setLang] = useState("pl");
 
   return (
     <ThemeContext.Provider value={{ theme, setTheme }}>
       {children}
     </ThemeContext.Provider>
+    <LangContext.Provider value={{lang, setLang}}>
+        {children}
+    </LangContext.Provider>
   );
 };
 
@@ -22,6 +30,17 @@ const ThemeSwitcher = () => {
       <p>Current Theme: {theme}</p>
       <button onClick={() => setTheme(theme === "light" ? "dark" : "light")}>
         Toggle Theme
+      </button>
+    </div>
+  );
+};
+const LangSwitcher = () => {
+  const { lang, setLang } = useContext(LangContext);
+  return (
+    <div>
+      <p>Current Language: {lang}</p>
+      <button onClick={() => setLang(lang === "pl" ? "en" : "pl")}>
+        Toggle Language
       </button>
     </div>
   );
